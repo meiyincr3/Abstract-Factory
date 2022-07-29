e4dc33b16fb029c33669b280d188d1387c3be783
Meiyin Chang <91926069+meiyincr3@users.noreply.github.com>
2022-07-29 11:10:25 -0500

    Delete src/main/java/com/mycompany/figurageometrica directory

--- src/main/java/com/mycompany/figurageometrica/Fabrica_Figuras.java
+++
@@ -1,9 +0,0 @@
-
-package FiguraGeometrica;
-
-public interface Fabrica_Figuras {
-    
-    public Bidimensional crearFiguraBidimensional(int x, int y, int lado);
-    public Tridimensional crearFiguraTridimensional(int x, int y, int lado);
-    
-}
